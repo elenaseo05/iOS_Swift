deec6c26da0031d500feed73d39b8bc33a4218d6
elenaseo05 <elenaseo05@gmail.com>
2022-01-21 09:49:51 +0900

    delete storyBoard

---
+++ CodeBaseUI/CodeBaseUI/HomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.swift
+//  CodeBaseUI
+//
+//  Created by Elena on 2022/01/21.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController : UICollectionViewController {
+    
+}
